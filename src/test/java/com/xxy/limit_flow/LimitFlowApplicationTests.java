f660797cc56d14d97db1d7d4e8a6c35fb7678912
moonKylin <xxy151694@163.com>
2023-07-10 02:09:54 +0800

    limitfl

---
+++ src/test/java/com/xxy/limit_flow/LimitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxy.limit_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
